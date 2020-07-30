8dc8cf6d9690ee2d74d821995c1822681b3e1d8f
maggiebarbie1995 <maggylangakush@gmail.com>
2020-07-30 17:05:16 +0300

    add textDEco which takes in an action and then performs a text-decoration using the action

--- quote/src/app/strikethrough.directive.ts
+++ quote/src/app/strikethrough.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef } from '@angular/core';
 export class StrikethroughDirective {
 
   constructor(private elem:ElementRef){
-    this.elem.nativeElement.style.textDecoration='line-through';
+    this.elem.nativeElement.style.textDecoration='action';
   }
 
 }
