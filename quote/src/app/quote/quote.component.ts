0fbf0cb93d72199aabbea1486317849e6bfd7f5a
maggiebarbie1995 <maggylangakush@gmail.com>
2020-07-30 16:06:08 +0300

    add file changes

--- quote/src/app/quote/quote.component.ts
+++ quote/src/app/quote/quote.component.ts
@@ -8,18 +8,18 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    {id:1, name:'Catherine Pulsifer'},
-    {id:2,name:'Nathan Arthur,Mission Statement'},
-    {id:3,name:'Max Strom,A Life Worth Breathing'},
-    {id:4,name:'R. Morrison,Parenting'},
-    {id:5,name:'Author Unknown'},
-    {id:6,name:'David Leads,Relationship Up'},
-    {id:7,name:'John Locke'},
-    {id:8,name:'Seneca'},
-    {id:9,name:'Bernadette Dimitrov'},
-    {id:10,name:'Giorgio Armani'}
+    new Quote(1,'Catherine Pulsifer','What you have accomplished in the past is a much stronger example than talking about what you are capable of doing in the future. Actions do speak louder than words!'),
+    new Quote(2,'Nathan Arthur,Mission Statement','Draw inspiration from other peoples lives The inspiring example of the people you admire is a great place to start with.'),
+    new Quote(3,'Max Strom,A Life Worth Breathing','May we all become teachers of peace, and teach in the only way possible: by example.'),
+    new Quote(4,'R. Morrison,Parenting','Around the age of one your child begins to imitate you with intent, they actively observe how you behave and seek clues in order to determine how they themselves should behave - thats a lot of pressure on you!'),
+    new Quote(5,'Author Unknown','Be careful how you live; you may be the only Bible some people ever read.'),
+    new Quote(6,'David Leads,Relationship Up','One of the best ways to teach a child anything is by doing it yourself. Leading by example requires no scolding or complicated instructions. Your child will see what you do and copy it.'),
+    new Quote(7,'John Locke','Manners are rather to be learnt by example than by rules.'),
+    new Quote(8,'Seneca','It is difficult to bring people to goodness with lessons, but it is easy to do so by example.'),
+    new Quote(9,'Bernadette Dimitrov','Remember that you cannot truly change anyone else you can only change yourself. You either positively or negatively influence others by your example.'),
+    new Quote(10,'Giorgio Armani','I love things that age well - things that dont date, that stand the test of time and that become living examples of the absolute best.')
   ];
-  
+
   constructor() { }
 
   ngOnInit(): void {
