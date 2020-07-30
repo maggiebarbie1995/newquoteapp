5ef4dc1b3303174da1fd679ed198a8b03f777435
maggiebarbie1995 <maggylangakush@gmail.com>
2020-07-30 14:28:57 +0300

    add array of quotes each with its ID and NAME

--- quote/src/app/app.component.ts
+++ quote/src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-
+import { Quote } from './quote';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes:string[];
-
-  constructor(){
-    this.quotes = ['Watch finding Nemo', 'Buy cookies', 'Get new phone case']
-  } 
+  quotes:Quote[] = [
+  {id:1, name:'Catherine Pulsifer'},
+  {id:2,name:'Nathan Arthur,Mission Statement'},
+  {id:3,name:'Max Strom,A Life Worth Breathing'},
+  {id:4,name:'R. Morrison,Parenting'},
+  {id:5,name:'Author Unknown'},
+  {id:6,name:'David Leads,Relationship Up'},
+  {id:7,name:'John Locke'},
+  {id:8,name:'Seneca'},
+  {id:9,name:'Bernadette Dimitrov'},
+  {id:10,name:'Giorgio Armani'}
+];
 }
+  
